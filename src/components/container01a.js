ea4ef8d78a94bcfe3d8dce7ab928d87fca06d1e0
salem <salem@laCosa>
2020-01-20 23:22:28 -0800

    yarn upgraded

--- src/components/container01a.js
+++ src/components/container01a.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import container01Styles from '../css/container01.module.css'
+//import '../css/main.css'
 import ResponsivePlayer01 from './responsiveplayer01'
 import TContentCards01 from './tcontentcardsrow1'
 import TContentCards02 from './tcontentcardsrow2'
@@ -17,26 +18,22 @@ class Container01a extends React.Component {
       return (
       <div id="container01" className="container columns full screen" data-scroll-id="one" data-scroll-behavior="center"  /*className={container01Styles.columns} className={container01Styles.full} className={container01Styles.full.screen}*/>
           <div className={container01Styles.inner}> 
-            <div className={container01Styles.afterSpacer}>
+            {/*<div className={container01Styles.afterSpacer}>*/}
                 
               <h2 id="text01" className={container01Styles.style1}>Feed The People</h2>
               {/**<hr id="divider03" />**/}
-              <p id="text02" className={container01Styles.style2}>Morbi semper nisl et dictum cursus. In hac habitasse platea dictumst. </p>
-              {/**<ResponsivePlayer01 />**/}
-              {/*<ReactWebMediaPlayer
-	            width={500} height={350}
-	            title="Chemtrails"
-	            thumbnail={Chemtrails}
-	            audio={Chemtrailsmp3}
-	            logo={{
-		            img: "../../images/logo-white.png",
-		            href: "#"
-	                }}
-                />*/}
-              {/*<Mediaplayer01 />*/}
+              {/*<p id="text02" className={container01Styles.style2}>Morbi semper nisl et dictum cursus. In hac habitasse platea dictumst. </p>*/}
+              <row style={{
+                  display: 'flex',
+              }}>
+              <container>
               <Visualizer />
-            </div>
+            {/*</div>*/}
             
+              
+                  <h1>Test</h1>
+              </container>
+            </row>
               <br />
               <hr />
           </div>
